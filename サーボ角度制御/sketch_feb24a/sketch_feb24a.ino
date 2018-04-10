37366eb076ebe22baf8397f332633a5f0f73a9e4
Hirokazu Miyoshi <38253944+minnya@users.noreply.github.com>
2018-04-10 23:58:52 +0900

    Add files via upload

---
+++ サーボ角度制御/sketch_feb24a/sketch_feb24a.ino
@@ -0,0 +1,23 @@
+#include<TimerOne.h>
+#include<LiquidCrystal.h>
+int sig[3] = {0}, i, moji[3];
+void flash() {
+  static boolean sw=HIGH;
+  if(i==0)tone(9, 40000);
+  else noTone(9);
+  i++;
+  if(i>3)i=0;
+}
+
+void setup() {
+  pinMode(9, OUTPUT);
+  pinMode(8, INPUT);
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+  Timer1.initialize(5000);
+  Timer1.attachInterrupt(flash);
+}
+void loop() {
+  if (digitalRead(8) == LOW) digitalWrite(13, HIGH);
+    else digitalWrite(13, LOW);
+    }
